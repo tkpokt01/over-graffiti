1620afe9fdd12548d97f7bb28f1999a2f42382e7
tkpokt <tkpokt@hotmail.com>
2025-01-26 02:30:52 +0900

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+// src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // React 18 uses ReactDOM.createRoot
+import App from './App'; // Import the App component
+
+// Create a root element and render the App component
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
---
+++ src/utils/contract.js
@@ -0,0 +1,102 @@
+import { ethers } from 'ethers';
+
+const contractAddress = '0xa3bd22f59065d49da39c7b99c3bd0d5f3dca93f8';
+const contractABI = [ [
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "sender",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "content",
+				"type": "string"
+			}
+		],
+		"name": "MessageWritten",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_content",
+				"type": "string"
+			}
+		],
+		"name": "writeMessage",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "index",
+				"type": "uint256"
+			}
+		],
+		"name": "getMessage",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			},
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getMessageCount",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "messages",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "sender",
+				"type": "address"
+			},
+			{
+				"internalType": "string",
+				"name": "content",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+] ];
+
+export const getContract = (provider) => {
+    return new ethers.Contract(contractAddress, contractABI, provider.getSigner());
+};
